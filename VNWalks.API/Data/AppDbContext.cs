5242ff85d8d6a2ddd9b55deee7fb0a306d9ad964
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-01-10 18:00:25 +0700

    Create Domain Models, Migration

---
+++ VNWalks.API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VNWalks.API.Models.Domain;
+
+namespace VNWalks.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+    }
+}
