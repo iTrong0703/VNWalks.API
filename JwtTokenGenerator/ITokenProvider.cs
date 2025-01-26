d0e4032e19324a126346236658f2db8a0ab83129
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-01-26 15:10:25 +0700

    Remove ignored files from repository

---
+++ JwtTokenGenerator/ITokenProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtTokenGenerator
+{
+    public interface ITokenProvider
+    {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
