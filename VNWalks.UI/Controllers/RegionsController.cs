f6a755cc4bb260bc2a590fa673869d8bdb1dbb9a
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-02-03 18:21:53 +0700

    create a UI project for testing API call

---
+++ VNWalks.UI/Controllers/RegionsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VNWalks.UI.Models.DTO;
+
+namespace VNWalks.UI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("https://localhost:7095/api/regions");
+                httpResponseMessage.EnsureSuccessStatusCode();
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+                ViewBag.Response = response;
+            }
+            catch (Exception ex)
+            {
+
+            } 
+            return View(response);
+        }
+    }
+}
---
+++ VNWalks.UI/Models/DTO/RegionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace VNWalks.UI.Models.DTO
+{
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
